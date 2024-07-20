6acf1d3acf45fc642af4ab2c385aa79e24874f68
wa1tnr <wa1tnr@users.noreply.github.com>
2024-07-20 04:29:05 +0000

    Name similar things similarly
    	modified:   main.c
    
     On branch dvlp-aa

--- rev-bb-stl/src/main.c
+++ rev-bb-stl/src/main.c
@@ -49,7 +49,12 @@ void blink() {
 
 void GPIOC_MODER_bang(void) {
     GPIOC->MODER |= GPIO_MODER_MODE6_1 | GPIO_MODER_MODE7_1;
-    GPIOC->OSPEEDR |= GPIO_OSPEEDR_OSPEED6 | GPIO_OSPEEDR_OSPEED7;
+}
+
+// void GPIOD__MODER____GPIO_MODER_MODER15_0() {
+void GPIOD_MODER_bang(void) {
+    /* PD15 is Blue LED - all 4x on PORT D pins 12-15 */
+    GPIOD->MODER |= GPIO_MODER_MODER15_0;
 }
 
 void SETUP_USART6_CR1(void) {
@@ -78,12 +83,8 @@ void largest_USART6(void) {
     enable_USART6();
     SET_GPIOC_MODER_PC6_PC7_ALT_A();
     GPIOC_MODER_bang();
+    GPIOC->OSPEEDR |= GPIO_OSPEEDR_OSPEED6 | GPIO_OSPEEDR_OSPEED7;
     SETUP_USART6_CR1();
-}
-
-void GPIOD__MODER____GPIO_MODER_MODER15_0() {
-    /* PD15 is Blue LED - all 4x on PORT D pins 12-15 */
-    GPIOD->MODER |= GPIO_MODER_MODER15_0;
 }
 
 // c-lang-format off
@@ -95,7 +96,8 @@ void enable_RCC_AHB1ENR_GPIOD() {
 
 void primary(void) {
     enable_RCC_AHB1ENR_GPIOD();
-    GPIOD__MODER____GPIO_MODER_MODER15_0();
+    // GPIOD__MODER____GPIO_MODER_MODER15_0();
+    GPIOD_MODER_bang();
     GPIOD->OTYPER = 0;
     GPIOD->OSPEEDR = 0;
     enable_RCC_AHB1ENR_GPIOC();
