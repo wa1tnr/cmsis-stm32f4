19f018046939da243ca4aecfd22490502ca2a5b3
wa1tnr <wa1tnr@users.noreply.github.com>
2024-07-20 22:59:11 +0000

    factor purge comments
    	modified:   main.c
    
    On branch dvlp-aa

--- rev-bb-stl/src/main.c
+++ rev-bb-stl/src/main.c
@@ -1,31 +1,14 @@
+/* main.c */
+/* Sat 20 Jul 02:48:47 UTC 2024 */
+
+/* USART6 enable and write-only (no listener) */
+/* port:  Forth source to C language */
+
+/* utilise: CMSIS notation and paradigm */
 
 /****
- *
- * Sat 20 Jul 02:48:47 UTC 2024
- * Wed 17 Jul 05:51:07 UTC 2024
- *
- * STATUS - although the code was guesswork, at a minimum,
- * it is operational (the blue LED continues to blink after
- * the 'fiasco' section of the code WRT USART6).
- *
- *    monitor() is now called after the USART6 routines (the
- *    function, largest_USART6() .. which calls all else wrt
- *    USART6 setups).
- *
- */
-
-/***
- *
  * PD15  LD6  Blue
- *
  * PD12  LD4  Green  #  PD13 LD3  Orange  # PD14 LD5  Red
- *
- ***/
-
-/***
- *
- * NEWS - blue LED stm32f407 Discovery now blinks blue LED.
- *
  ***/
 
 #include <stdbool.h>
@@ -51,25 +34,28 @@ void GPIOC_MODER_bang(void) {
     GPIOC->MODER |= GPIO_MODER_MODE6_1 | GPIO_MODER_MODE7_1;
 }
 
-// void GPIOD__MODER____GPIO_MODER_MODER15_0() {
 void GPIOD_MODER_bang(void) {
     /* PD15 is Blue LED - all 4x on PORT D pins 12-15 */
     GPIOD->MODER |= GPIO_MODER_MODER15_0;
 }
 
+void set_USART6_BRR(void) {
+    USART6->BRR = 0x138; /* 115200 */
+}
+
 void SETUP_USART6_CR1(void) {
     USART6->CR1 &= ~USART_CR1_UE;
     USART6->CR1 |= USART_CR1_RE;
-    USART6->BRR = 0x138; /* 115200 */
+    set_USART6_BRR();
+    // USART6->BRR = 0x138; /* 115200 */
     USART6->CR1 |= USART_CR1_UE;
 }
 
-void SET_GPIOC_MODER_PC6_PC7_ALT_A() {
+void map_AFR_USART6() {
     GPIOC->AFR[0] |=
         (0x8UL << GPIO_AFRL_AFSEL6_Pos) | (0x8UL << GPIO_AFRL_AFSEL7_Pos);
 }
 
-// clang-format off
 void enable_USART6() {
     RCC->APB2ENR |= RCC_APB2ENR_USART6EN;
 }
@@ -81,14 +67,10 @@ void enable_RCC_AHB1ENR_GPIOC() {
 void enable_RCC_AHB1ENR_GPIOD() {
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
 }
-// clang-format on
-
-/* TODO: give this a better name than largest foo */
-
 void largest_USART6(void) {
     enable_RCC_AHB1ENR_GPIOC();
     enable_USART6();
-    SET_GPIOC_MODER_PC6_PC7_ALT_A();
+    map_AFR_USART6();
     GPIOC_MODER_bang();
     GPIOC->OSPEEDR |= GPIO_OSPEEDR_OSPEED6 | GPIO_OSPEEDR_OSPEED7;
     SETUP_USART6_CR1();
