aa85147a42a5d4f2ab998429db02722a3194366b
wa1tnr <wa1tnr@users.noreply.github.com>
2024-07-20 03:27:15 +0000

    include file for delays dot c use

---
+++ rev-bb-stl/src/delays.h
@@ -0,0 +1,12 @@
+/****
+ * Sat 20 Jul 02:48:47 UTC 2024 *
+ */
+
+extern void blink_once();
+extern void slowest();
+extern void blinkDelayOffToOn();
+extern void sayHi();
+extern void ldelayed();
+extern void monitor();
+
+/* end. */
