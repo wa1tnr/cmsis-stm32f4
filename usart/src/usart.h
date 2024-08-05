6b65687ddf7b210f137ddf3f20369711fbbbb3f7
wa1tnr <wa1tnr@users.noreply.github.com>
2024-08-05 23:29:36 +0000

    restore two files second step

---
+++ usart/src/usart.h
@@ -0,0 +1,13 @@
+/* usart.h */
+/* Sat  3 Aug 16:48:19 UTC 2024 */
+
+#ifndef USART_HEADER_FILE
+#define USART_HEADER_FILE
+extern void initGPIO(void);
+extern void quickBlinks();
+extern void initUSART6(void);
+extern void printTestMessage();
+extern void lnthyWSpaceIval();
+#endif
+
+/* end. */
